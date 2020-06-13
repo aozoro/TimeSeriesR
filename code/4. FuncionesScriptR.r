8340eb331143f9366b51032a15e6eda7e72f78c5
Omar de la Sota <aozoro@gmail.com>
2020-06-12 19:26:05 -0500

    Create 4. FuncionesScriptR.r

---
+++ code/4. FuncionesScriptR.r
@@ -0,0 +1,30 @@
+saludo <- function(){
+    print("buenos dias")
+}
+
+saludo()
+saludo()
+
+##hipotenusa segun teorema de pitagoras
+
+#comprobar que la hipotenusa de los catetos 3 y 4 es 5
+
+#implementar la funcion llamada circulo que tenga como argumento el radio y nos devuelva: diametro, area y longitud de arco
+
+#ejecutar la funcion circulo con un radio 1
+
+#asignar a la variable x los output de un circulo de radio 2. Y solo mostrar en pantalla el área del cirulo 
+
+circulo()
+
+#Crear la funcion saludo_personal que tenga como argumento el nombre, en caso no se ingrese nombre poner "Usuario". Y saludar "hola, nombre"
+
+# ejecutar con tu nombre
+
+#ejecutar sin nombre
+
+source("Ejemplo.R")
+
+#utilizar source
+
+# probar con el vector [1,2,3,4]
